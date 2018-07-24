54356ffcd09d769e8d015d81306b126464c1b799
wangkaiwd <1162486176@qq.com>
2018-07-24 21:13:28 +0800

    完成倒计时，并且select组件样式书写完毕

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,12 @@
         >
         </base-paging>
       </el-tab-pane>
-      <el-tab-pane label="消息中心">消息中心</el-tab-pane>
-      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+      <el-tab-pane label="倒计时">
+        <base-timeout></base-timeout>
+      </el-tab-pane>
+      <el-tab-pane label="下拉菜单">
+        <base-select></base-select>
+      </el-tab-pane>
       <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
     </el-tabs>
     <!-- <router-view/> -->
@@ -19,41 +23,46 @@
 </template>
 
 <script>
-import BasePaging from "@/components/Paging";
-export default {
-  // name属性：
-  // 1. 允许组件模板递归的调用自身
-  // 2. 便于调试，有名字的组件有更友好的警告信息
-  // 3. 当在有vue-devtools,未命名组件将显示成<AnonymousComponent>,很没有语义，通过name选项，可以获得更有语义信息的组件树
-  name: "App",
-  data() {
-    return {
-      total: 100,
-      pageSize: 5,
-      currentPage: 1
-    };
-  },
-  components: {
-    BasePaging
-  },
-  methods: {
-    change(val) {
-      this.currentPage = val;
+  import BasePaging from '@/components/Paging'
+  import BaseTimeout from '@/components/timeDown'
+  import BaseSelect from '@/components/selfSelect'
+
+  export default {
+    // name属性：
+    // 1. 允许组件模板递归的调用自身
+    // 2. 便于调试，有名字的组件有更友好的警告信息
+    // 3. 当在有vue-devtools,未命名组件将显示成<AnonymousComponent>,很没有语义，通过name选项，可以获得更有语义信息的组件树
+    name: 'App',
+    data () {
+      return {
+        total: 100,
+        pageSize: 5,
+        currentPage: 1
+      }
+    },
+    components: {
+      BasePaging,
+      BaseTimeout,
+      BaseSelect
+    },
+    methods: {
+      change (val) {
+        this.currentPage = val
+      }
     }
   }
-};
 </script>
 
 <style lang="less">
-#app {
-  height: 100%;
-  .el-tabs--border-card {
-    display: flex;
-    flex-direction: column;
+  #app {
     height: 100%;
+    .el-tabs--border-card {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+    .el-tabs--border-card > .el-tabs__content {
+      flex: 1;
+    }
   }
-  .el-tabs--border-card > .el-tabs__content {
-    flex: 1;
-  }
-}
 </style>
