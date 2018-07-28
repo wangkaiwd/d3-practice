a66c87fb707b45739a80b6447e820b59d950c713
wangkaiwd <1162486176@qq.com>
2018-07-28 23:46:09 +0800

    倒计时组件基本功能实现

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,15 @@
       </el-tab-pane>
       <el-tab-pane label="倒计时">
         <base-timeout></base-timeout>
+        <div style="marginTop:12px;">
+
+        </div>
+        <new-set-timeout :isOpenCountDown.sync="isOpenCountDown"
+                         @click="handleClick"
+                         initText="点击发送发送验证码"
+                         executeText="请等待20s"
+                         :number="20"
+                         :checkOptions="checkOptions"></new-set-timeout>
       </el-tab-pane>
       <el-tab-pane label="下拉菜单">
         <base-select></base-select>
@@ -26,6 +35,7 @@
   import BasePaging from '@/components/Paging'
   import BaseTimeout from '@/components/timeDown'
   import BaseSelect from '@/components/selfSelect'
+  import NewSetTimeout from '@/components/timeDown/new'
 
   export default {
     // name属性：
@@ -37,17 +47,26 @@
       return {
         total: 100,
         pageSize: 5,
-        currentPage: 1
+        currentPage: 1,
+        isOpenCountDown: false,
+        checkOptions: {
+          text: 'email',
+          number: 18404713626
+        }
       }
     },
     components: {
       BasePaging,
       BaseTimeout,
-      BaseSelect
+      BaseSelect,
+      NewSetTimeout
     },
     methods: {
       change (val) {
         this.currentPage = val
+      },
+      handleClick () {
+        this.isOpenCountDown = true
       }
     }
   }
--- src/components/timeDown/new.vue
+++ src/components/timeDown/new.vue
@@ -1,87 +1,89 @@
 <template>
-  <button :disabled="disabled">{{text}}</button>
+  <div class="new-settimeout">
+    <el-button :disabled="disabled"
+               @click="$emit('click')"
+               type="primary">
+      {{text}}
+    </el-button>
+  </div>
 </template>
 
 <script>
-// import { XButton } from "vux";
-// import { validateEmail, validMobile } from "@/utils/validate";
+  // import { validateEmail, validMobile } from "@/utils/validate";
 
-export default {
-  name: "SetTimeout",
-  props: {
-    isOpenCountDown: {
-      type: Boolean,
-      default: false
+  export default {
+    name: 'NewSetTimeout',
+    props: {
+      isOpenCountDown: {type: Boolean, default: false},
+      initText: {type: String, default: '获取'},
+      executeText: {type: String, default: '60s'},
+      number: {type: Number, default: 60},
+      checkOptions: {
+        type: Object,
+        default () {
+          return {}
+        },
+        validator (val) {
+          return ['phone', 'email', undefined].indexOf(val.text) > -1
+        }
+      }
     },
-    initText: {
-      type: String,
-      default: "获取"
+    data () {
+      return {disabled: false, text: this.initText}
     },
-    executeText: {
-      type: String,
-      default: "60s"
+    watch: {
+      isOpenCountDown (val) {
+        val ? this.countDown() : clearInterval(this.timerId)
+      }
     },
-    number: {
-      type: Number,
-      default: 60
-    },
-    checkOptions: {
-      type: Object,
-      default() {
-        return {};
+    methods: {
+      infoChecked () {
+        let valid = true
+        const {text, number} = this.checkOptions
+        if (text === 'email') {
+          // valid = validateEmail(number);
+          console.log('合法email')
+        }
+        if (text === 'phone') {
+          // valid = validMobile(number);
+          console.log('合法phone')
+        }
+        return valid
       },
-      validator(val) {
-        return ["phone", "email", undefined].indexOf(val.text) > -1;
-      }
-    }
-  },
-  data() {
-    return {
-      disabled: false,
-      text: this.initText
-    };
-  },
-  components: {
-    XButton
-  },
-  watch: {
-    isOpenCountDown(val) {
-      val ? this.countDown() : clearInterval(this.timerId);
-    }
-  },
-  methods: {
-    infoChecked() {
-      let valid = true;
-      const { text, number } = this.checkOptions;
-      if (text === "email") {
-        // valid = validateEmail(number);
-        console.log("email");
-      }
-      if (text === "phone") {
-        // valid = validMobile(number);
-        console.log("phone");
-      }
-      return valid;
+      countDown () {
+        let {number} = this
+        if (this.infoChecked()) {
+          clearInterval(this.timerId)
+          this.disabled = true
+          this.timerId = setInterval(() => {
+            if (number <= 0) {
+              clearInterval(this.timerId)
+              this.$emit('update:isOpenCountDown', false)
+              this.disabled = false
+              this.text = this.initText
+              return
+            }
+            this.text = this.replaceNumber(number)
+            --number
+          }, 1000)
+        } else {
+          this.$emit('update:isOpenCountDown', false)
+        }
+      },
+      replaceNumber (number) {
+        const reg = /\d+/
+        return this.executeText.replace(reg, number.toString())
+      },
     },
-    countDown() {
-      let { number } = this;
-      if (this.infoChecked()) {
-        clearInterval(this.timerId);
-        this.disabled = true;
-        this.timerId = setInterval(() => {
-          if (number <= 0) {
-            clearInterval(this.timerId);
-            this.$emit("changeStatus", false);
-            this.disabled = false;
-            this.text = this.initText;
-            return;
-          }
-          this.text = `${number}s`;
-          --number;
-        }, 1000);
-      } else {
-        this.$emit("changeStatus", false);
-      }
-    }
   }
-};
+  /**
+   * String.prototype.match():
+   *  当一个字符串与一个正则表达式匹配时，`match()`方法检索匹配项
+   *
+   * String.prototype.replace():
+   *  方法返回一个由替换值替换一些或所有匹配的模式后的新字符串
+   */
+</script>
+<style lang="less" scoped>
+  .new-settimeout {display: inline-block;}
+</style>
