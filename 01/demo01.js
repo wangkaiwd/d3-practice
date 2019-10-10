de1ed690548b62c99c182f7a32531b0afee91a40
wangkaiwd <1162486176@qq.com>
2019-10-10 16:05:51 +0800

    feat: dynamic create circle tag

--- 01/demo01.js
+++ 01/demo01.js
@@ -1,18 +1,46 @@
 "use strict";
-exports.__esModule = true;
-var d3_1 = require("d3");
-// Select the svg area
-var svg = d3_1["default"].select('#Viz_area');
-// Create a scale: transform value in pixel
-var x = d3_1["default"].scaleLinear()
-    .domain([0, 100]) // This is the min and the max of the data: 0 to 100 if percentages
-    .range([50, 400]); // This is the corresponding value I want in Pixel
-// Show the axis that corresponds to this scale
-svg.call((d3_1["default"].axisBottom(x)));
-// Add 3 dots for 0, 50 and 100%
-svg.append('circle')
-    .attr('cx', x(10)).attr('cy', 100).attr('r', 40).style('fill', 'blue');
-svg.append('circle')
-    .attr('cx', x(50)).attr('cy', 100).attr('r', 40).style('fill', 'red');
-svg.append('circle')
-    .attr('cx', x(100)).attr('cy', 100).attr('r', 40).style('fill', 'green');
+/**
+ * <circle></circle>
+ * cx: 定义一个中心点的x轴坐标
+ * cy: 定义一个中心点的y轴坐标
+ * r : 用来定义圆的半径
+ *
+ * x,y轴坐标相对于父元素
+ *
+ * selection.data: 指定一组数据和对应于选择的元素
+ *
+ * data中的值的每一项会作为调用`api`中传入函数的第一个参数，第二个参数为当前元素的索引
+ */
+// const svg = d3.select('svg');
+// const circle = svg.selectAll('circle');
+// set circle.style
+// circle.style('fill', 'steelblue');
+// attr , data, second callback function parameters
+// function first param: item in data, second param: element index in all elements
+// circle
+//   .data([32, 57, 112])
+//   .attr('fill', 'steelblue')
+//   .attr('r', (d) => Math.sqrt(d))
+//   .attr('cx', (d, i) => {
+//     console.log(d, i); //
+//     return 40 * (i + 1);
+//   });
+// enter: 暂时不存在的DOM元素中的每个数据元素的占位符节点
+// const svg = d3.select('svg');
+//
+// const circle = svg.selectAll('circle')
+//   .data([32, 57, 112, 293]);
+//
+// const circleEnter = circle.enter().append('circle');
+// circleEnter.attr('cy', 60)
+//   .attr('cx', 160)
+//   .attr('r', function (d) { return Math.sqrt(d); });
+// common use combination: selectAll + data + enter + append
+// use a svg dynamic create circle what the number of it correspond of data
+var svg = d3.select('svg');
+svg.selectAll('circle')
+    .data([32, 57, 112, 293])
+    .enter().append('circle')
+    .attr('cy', 60)
+    .attr('cx', function (d, i) { return i * 100 + 30; })
+    .attr('r', function (d) { return Math.sqrt(d); });
--- 01/demo01.ts
+++ 01/demo01.ts
@@ -1,19 +1,50 @@
-import d3 from 'd3';
-// Select the svg area
-const svg = d3.select('#Viz_area');
+/**
+ * <circle></circle>
+ * cx: 定义一个中心点的x轴坐标
+ * cy: 定义一个中心点的y轴坐标
+ * r : 用来定义圆的半径
+ *
+ * x,y轴坐标相对于父元素
+ *
+ * selection.data: 指定一组数据和对应于选择的元素
+ *
+ * data中的值的每一项会作为调用`api`中传入函数的第一个参数，第二个参数为当前元素的索引
+ */
+// const svg = d3.select('svg');
+// const circle = svg.selectAll('circle');
+// set circle.style
+// circle.style('fill', 'steelblue');
 
-// Create a scale: transform value in pixel
-const x = d3.scaleLinear()
-  .domain([0, 100])         // This is the min and the max of the data: 0 to 100 if percentages
-  .range([50, 400]);       // This is the corresponding value I want in Pixel
+// attr , data, second callback function parameters
+// function first param: item in data, second param: element index in all elements
+// circle
+//   .data([32, 57, 112])
+//   .attr('fill', 'steelblue')
+//   .attr('r', (d) => Math.sqrt(d))
+//   .attr('cx', (d, i) => {
+//     console.log(d, i); //
+//     return 40 * (i + 1);
+//   });
 
-// Show the axis that corresponds to this scale
-svg.call((d3.axisBottom(x)) as any);
+// enter: 暂时不存在的DOM元素中的每个数据元素的占位符节点
+// const svg = d3.select('svg');
+//
+// const circle = svg.selectAll('circle')
+//   .data([32, 57, 112, 293]);
+//
+// const circleEnter = circle.enter().append('circle');
+// circleEnter.attr('cy', 60)
+//   .attr('cx', 160)
+//   .attr('r', function (d) { return Math.sqrt(d); });
 
-// Add 3 dots for 0, 50 and 100%
-svg.append('circle')
-  .attr('cx', x(10)).attr('cy', 100).attr('r', 40).style('fill', 'blue');
-svg.append('circle')
-  .attr('cx', x(50)).attr('cy', 100).attr('r', 40).style('fill', 'red');
-svg.append('circle')
-  .attr('cx', x(100)).attr('cy', 100).attr('r', 40).style('fill', 'green');
+// common use combination: selectAll + data + enter + append
+
+// use a svg dynamic create circle what the number of it correspond of data
+const svg = d3.select('svg');
+
+svg.selectAll('circle')
+  .data([32, 57, 112, 293])
+  .enter().append('circle')
+  .attr('cy', 60)
+  .attr('cx', (d, i) => i * 100 + 30)
+  .attr('r', d => Math.sqrt(d));
